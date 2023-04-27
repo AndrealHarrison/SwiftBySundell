ec491117287eb135304ae498d1778e424f539013
AndrealHarrison <andrealharrison@gmail.com>
2023-04-26 20:51:04 -0700

    added function to remove product from cart and unit test for it

--- Basics/unit-testing.playground/Contents.swift
+++ Basics/unit-testing.playground/Contents.swift
@@ -5,7 +5,19 @@
 
 import XCTest
 
-// --- Testing mutating a model ---
+// --- Product and Coupon Models ---
+
+struct Product: Equatable {
+    let name: String
+    var price: Double
+}
+
+struct Coupon {
+    let name: String
+    let discount: Double
+}
+
+// --- Product Extension ---
 
 extension Product {
     mutating func apply(_ coupon: Coupon) {
@@ -13,6 +25,30 @@ extension Product {
         price *= multiplier
     }
 }
+
+// --- ShoppingCart Class ---
+
+class ShoppingCart {
+    private var products: [Product] = []
+
+    var totalPrice: Double {
+        return products.reduce(0) { (result, product) -> Double in
+            return result + product.price
+        }
+    }
+
+    func add(_ product: Product) {
+        products.append(product)
+    }
+
+    func remove(_ productToRemove: Product) {
+        if let index = products.firstIndex(where: { $0 == productToRemove }) {
+            products.remove(at: index)
+        }
+    }
+}
+
+// --- Unit Tests ---
 
 class ProductTests: XCTestCase {
     func testApplyingCoupon() {
@@ -28,23 +64,16 @@ class ProductTests: XCTestCase {
     }
 }
 
-// --- Using a test case's setUp method ---
-
 class ShoppingCartTests: XCTestCase {
-    // Normally, it can be argued that force unwrapping (!) should
-    // be avoided, but in unit tests it can be a good idea for
-    // properties (only!) in order to avoid unnecessary boilerplate.
     private var shoppingCart: ShoppingCart!
 
     override func setUp() {
         super.setUp()
-        // Using this, a new instance of ShoppingCart will be created
-        // before each test is run.
         shoppingCart = ShoppingCart()
     }
 
     func testCalculatingTotalPrice() {
-        // Given: Here we assert that our initial state is correct
+        // Given
         XCTAssertEqual(shoppingCart.totalPrice, 0)
 
         // When
@@ -54,9 +83,23 @@ class ShoppingCartTests: XCTestCase {
         // Then
         XCTAssertEqual(shoppingCart.totalPrice, 35)
     }
+
+    func testRemovingProduct() {
+        // Given
+        let book = Product(name: "Book", price: 20)
+        let movie = Product(name: "Movie", price: 15)
+        shoppingCart.add(book)
+        shoppingCart.add(movie)
+        XCTAssertEqual(shoppingCart.totalPrice, 35)
+
+        // When
+        shoppingCart.remove(book)
+
+        // Then
+        XCTAssertEqual(shoppingCart.totalPrice, 15)
+    }
 }
 
 // --- Running all of our unit tests within the playground ---
 
-// Check Xcode's console for details
 XCTestSuite.default.run()
